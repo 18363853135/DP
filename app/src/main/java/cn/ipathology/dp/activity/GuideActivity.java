80317fd843688d2d19440f937b14029463f3c59b
1173964585@qq.com <1173964585@qq.com>
2017-01-03 17:57:14 +0800

    第一次提交

--- app/src/main/java/cn/ipathology/dp/activity/GuideActivity.java
+++ app/src/main/java/cn/ipathology/dp/activity/GuideActivity.java
@@ -212,8 +212,8 @@ public class GuideActivity extends Activity implements GestureDetector.OnGesture
 //                intent.putExtra("value", "调用成功");
 //                //  调用ActionActivity中的Main
 //                startActivity(intent);
-
-                if (!is2CallBack) {
+                https://git.oschina.net/1173964585/DP.git
+                              if (!is2CallBack) {
                     if (TextUtils.isEmpty(new TokenUtil().getToken())) {
                         Intent intent = new Intent(GuideActivity.this, LoginActivity.class);
                         startActivity(intent);
