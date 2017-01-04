90f0bae50e81e693cbfa81d01c49f91c297c85a5
1173964585@qq.com <1173964585@qq.com>
2017-01-04 09:16:50 +0800

    整理代码

--- app/src/main/java/cn/ipathology/dp/activity/base/BaseWebViewActivity.java
+++ app/src/main/java/cn/ipathology/dp/activity/base/BaseWebViewActivity.java
@@ -276,14 +276,11 @@ public class BaseWebViewActivity extends BaseActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
-
 
     //关闭当前的页面 并请求Js对上个页面进行操作
     public void appBridgeClose(String data) {
         try {
-            
             JSONObject jsonObject = new JSONObject(data);
             String functionName = jsonObject.getString("fnname");
             String param = jsonObject.getString("param");
